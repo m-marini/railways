1934697d3ba4f7fc7f8814622bc495bd797ad4da
m-marini <marco.marini@mmarini.org>
2015-04-17 00:44:46 +0200

    Built the start panel

--- railways3d/src/main/scala/org/mmarini/railways3d/Main.scala
+++ railways3d/src/main/scala/org/mmarini/railways3d/Main.scala
@@ -1,16 +1,28 @@
 package org.mmarini.railways3d
 
 import com.jme3.app.SimpleApplication
+import com.jme3.niftygui.NiftyJmeDisplay
 
 object Main extends SimpleApplication {
 
   /**
-   * 
+   *
    */
   @Override
   def simpleInitApp: Unit = {
-    val spatial = assetManager.loadModel("Interface/coatch.j3o")
-    rootNode.attachChild(spatial);
+    val niftyDisplay = new NiftyJmeDisplay(assetManager, inputManager, audioRenderer, guiViewPort)
+
+    // Create a new NiftyGUI object
+    val nifty = niftyDisplay.getNifty()
+
+    // Read your XML and initialize your custom ScreenController
+    nifty.fromXml("Interface/screen.xml", "start")
+
+    // attach the Nifty display to the gui view port as a processor
+    guiViewPort.addProcessor(niftyDisplay)
+
+    // disable the fly cam
+    flyCam.setDragToRotate(true)
   }
 
   /**
